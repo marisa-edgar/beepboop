6ae962b2d94808aba100b5b7a5a506f608a09729
marisa-edgar <marisaedgar1212@gmail.com>
2021-10-29 11:51:23 -0700

    WIP test 2

--- js/scripts.js
+++ js/scripts.js
@@ -9,9 +9,25 @@ function makeBeep(number) {
   }
   return beeper;
 }
-function beepboop() {
+function beepboop(number) {
   let beepboopArray = [];
   for(let i=1; i < 16; i++){
+    if (i === 3){
+      beepboopArray.push("wont you be my neighbor")
+    }else if(i === 2){
+      beepboopArray.push("boop");
+    } else if (i === 1) {
+      beepboopArray.push("beep");
+    } else {
+      beepboopArray.push(i);
+    }
+  }
+  return beepboopArray;
+}
+function beepboop(number) {
+  let numberArray = number.split('');
+  let beepboopArray = [];
+  for(let i=0; i < number.length; i++){
     if (i === 3){
       beepboopArray.push("wont you be my neighbor")
     }else if(i === 2){
