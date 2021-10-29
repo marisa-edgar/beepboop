dded516a2642be30b4395f6b435fd1645ea4154d
marisa-edgar <marisaedgar1212@gmail.com>
2021-10-29 10:21:31 -0700

    add imput box and submit button in html

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+function makeBeep(number) {
+  let beeper = [];
+  for(let i=1; i === 1; i++){
+    if (i === 1){
+      beeper.push('beep')
+    } else {
+      fizzBuzzArray.push(i);
+    }
+  }
+  return beeper;
+}
