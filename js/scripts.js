a3f3fc0056cd0e1d0a10747826c653d22eb6d4e3
marisa-edgar <marisaedgar1212@gmail.com>
2021-11-06 23:15:31 -0700

    fix naming conventions

--- js/scripts.js
+++ js/scripts.js
@@ -1,95 +1,29 @@
 //UI Logic
 
 $(document).ready(function(){
-  $("#formOne").submit(function(event){
+  $("form#formOne").submit(function(event){
     event.preventDefault();
-    const formInput = $("#formInput").val();
-    $("#output").text(pigLatin(formInput));
+    const formInput = $("input#input").val();
+    let result = beepboop(formInput)
+    $("#output").text(result);
   });
 });
 
 //Business logic
 
-function makeBeep(number) {
-  let beeper = [];
-  for(let i=1; i === 1; i++){
-    if (i === 1){
-      beeper.push('beep')
-    } else {
-      fizzBuzzArray.push(i);
-    }
-  }
-  return beeper;
-}
 function beepboop(number) {
-  let beepboopArray = [];
-  function numberLength(number) {
-    let length = 0;
-    let n = Math.abs(number);
-    do {
-      n /= 10;
-      length++;
-    } while (n >= 1);
-    return length;
-  }
-  for(let i=1; numberLength <= 16; i++){
-    if (i === 3){
-      beepboopArray.push("wont you be my neighbor")
-    }else if(i === 2){
+  let beepboopArray= [];
+    for(let i=0; i <= number; i++){
+      let beepboopString =i.toString();
+      if (beepboopString.includes("3")){
+      beepboopArray.push("wont ypu be my neighbor");
+    } else if (beepboopString.includes("2")){
       beepboopArray.push("boop");
-    } else if (i === 1) {
+    }else if(beepboopString.includes("1")){
       beepboopArray.push("beep");
     } else {
-      beepboopArray.push(i);
+      beepboopArray.push(" " + beepboopString);
     }
   }
   return beepboopArray;
-}
-
-function numberLength(number) {
-	let length = 0;
-	let n = Math.abs(number);
-	do {
-		n /= 10;
-		length++;
-	} while (n >= 1);
-	return length;
-}
-
-function beepboop(number) {
-  let beepboopArray = [];
-  for(let i=0; i < 16; i++){
-    if (i === 3){
-      beepboopArray.push("wont you be my neighbor")
-    }else if(i === 2){
-      beepboopArray.push("boop");
-    } else if (i === 1) {
-      beepboopArray.push("beep");
-    } else {
-      beepboopArray.push(i);
-    }
-  }
-  return beepboopArray;
-}
-const numbers = [1,2,3,4,5,6];
-const string = "beep";
-const stringArray = string.split(" ");
-let xArray = [];
-stringArray.forEach(function(letter) {
-  if (numbers.includes(letter)) {
-    xArray.push("beep");
-  } else {
-    xArray.push(letter);
-  }
-});
-const finalString = xArray.join(" ");
-
-ß
-function noInputtednumbers() {
-  for (let i=0; i < arguments.length; i++) {
-    if (arguments[i].trim().length === 0) {
-      return true;
-    }
-  }
-  return false;
 }
